7e68489a071ce61851165c977e2bf9885e0e1435
EC2 Default User <ec2-user@ip-172-31-6-29.ec2.internal>
2021-09-14 19:18:03 +0000

    [Add]インデックス画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'bookers' => 'homes#bookers'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' =>'books#edit'
+  post 'books' => 'books#create'
 end
